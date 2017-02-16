bf1d7ec2b299a07e464b3c3b3eead158ab8b54dc
gvukasin <gabrielle.vukasin@tufts.edu>
2017-02-15 23:15:11 -0800

    Add first Header file

---
+++ ActionService.h
@@ -0,0 +1,24 @@
+/****************************************************************************
+ Header file for MotorService
+   MotorService.c
+	 
+ Elena Galbally
+*****************************************************************************/
+
+#ifndef ActionService_H
+#define ActionService_H
+
+// Event Definitions
+#include "ES_Configure.h" /* gets us event definitions */
+#include "ES_Types.h"     /* gets bool type for returns */
+#include "ES_Events.h" 
+
+// Public Function Prototypes
+bool InitializeActionService(uint8_t Priority);
+bool PostActionService(ES_Event ThisEvent);
+ES_Event RunActionService(ES_Event ThisEvent);	
+void OneShotISR(void);
+void InputCaptureForIRDetectionResponse(void);
+void EnableIRInterrupt(void);
+
+#endif /* ActionService_H */
